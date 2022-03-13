101a1f463d2dd31f5c958545566844da960ee709
nathandbrowne <nathan.browne1@gmail.com>
2022-03-13 16:07:07 +0000

    add pymongo integration

---
+++ utils/mongo.py
@@ -0,0 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+
+class Mongo(MongoClient):
+    """
+    Really, the purpose of this class is just to hide the conn string but it is quite ugly
+    """
+
+    def __init__(self, db_name="cusum") -> None:
+
+        load_dotenv()
+        ATLAS_USER = os.environ.get("ATLAS_USER")
+        ATLAS_PASSWD = os.environ.get("ATLAS_PASSWD")
+
+        super().__init__(
+            "mongodb+srv://"
+            + ATLAS_USER
+            + ":"
+            + ATLAS_PASSWD
+            + "@moonfazer.p0pd0.mongodb.net/"
+            + db_name
+            + "?retryWrites=true&w=majority"
+        )
