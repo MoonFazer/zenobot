85faf0cd2727fbbdcbfe950784e7ce6bdcad9b4b
nathandbrowne <nathan.browne1@gmail.com>
2022-03-17 20:47:34 +0000

    add utils file for cusum process

---
+++ watcher/utils.py
@@ -0,0 +1,66 @@
+import itertools
+
+import pandas as pd
+from common.connection import get_connections
+
+
+def get_active_range(market, price):
+
+    """finds the active range of a crypto with pre-established support-resistance levels"""
+
+    mongo = get_connections("mongo", "cusum")
+    ranges_db = mongo["archive"].keyPriceLevels
+
+    market = market.split(f"/")[0]
+    price_list = [x for x in ranges_db.find({"name": market})][0]["levels"]
+    price_list = [float(x) for x in price_list]
+    low_bound = max([x for x in price_list if x < price])
+    high_bound = min([x for x in price_list if x >= price])
+    return (low_bound, high_bound)
+
+
+def reg_keys(entries):
+    """creates a dict of empty strings by unpacking a counter dictionary"""
+    big_set = set(itertools.chain(*entries))
+    return {key: "" for key in big_set}
+
+
+def to_user_catalog(records):
+
+    rows = []
+    for record in records:
+        for entry in record["watchList"]:
+            for agg in entry["aggs"]:
+                agg_type, agg_unit, agg_perc = agg.split("_")
+                rows.append(
+                    {
+                        "TGUsername": record["TGUsername"],
+                        "TGChatID": record["TGChatID"],
+                        "market": entry["market"],
+                        "agg_type": agg_type,
+                        "agg_unit": float(agg_unit),
+                        "agg_perc": float(agg_perc),
+                        "count": 0.0,
+                        "cusum_count": 0.0,
+                        "last_agg_price": None,
+                    }
+                )
+    return (
+        pd.DataFrame(rows)
+        .groupby(
+            [
+                "market",
+                "agg_type",
+                "agg_unit",
+                "agg_perc",
+            ],
+            as_index=False,
+        )
+        .agg(
+            users=("TGUsername", lambda x: list(set(x))),
+            ids=("TGChatID", lambda x: list(set(x))),
+            count=("count", lambda x: 0.0),
+            cusum_count=("cusum_count", lambda x: 0.0),
+            last_agg_price=("last_agg_price", lambda x: None),
+        )
+    )
